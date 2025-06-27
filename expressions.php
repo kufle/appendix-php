be74296301e0f71c77c89353285ad673525ec7c8
gudir <pengetahuanburung@gmail.com>
2025-06-27 20:57:24 +0700

    Add initial PHP examples for control structures and data types

---
+++ expressions.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Welcome to PHP\n";
+
+$name = "Alice";
+
+echo "Hello, ". $name . "\n";
+
+$pizzas = 3;
+$slicesPizza = 8;
+$totalSlices = $pizzas * $slicesPizza;
+
+echo "Total Pizza slices: ". $totalSlices ."\n";
+
+$isHungry = true;
+
+echo "Hungry?";
+echo $isHungry ? "Yes" : "No";
+echo "\n";
